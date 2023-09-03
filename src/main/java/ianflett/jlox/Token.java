cc367debe574a15260845b02e2c9bd749b7b6d0c
Ian Flett <ian.flett@gmail.com>
2023-09-03 21:55:22 +0100

    Lexemes and tokens

--- src/main/java/ianflett/jlox/Token.java
+++ src/main/java/ianflett/jlox/Token.java
@@ -1,4 +1,41 @@
 package ianflett.jlox;
 
-/** Temporary Token class for compilation. */
-public class Token {}
+/** Represents a {@link Token} (keyword, operator, punctuation, literal) in the Lox language. */
+class Token {
+
+    /** Stores the {@link Token}'s type. */
+    final TokenType type;
+
+    /** Stores the raw {@link Token} text. */
+    final String lexeme;
+
+    /** Stores the converted {@link Token} value. */
+    final Object literal;
+
+    /** Stores the line number location. */
+    final int line;
+
+    /**
+     * Constructs a {@link Token}.
+     *
+     * @param type The {@link Token}'s type.
+     * @param lexeme The raw {@link Token} text.
+     * @param literal The converted {@link Token} value.
+     * @param line The line number location.
+     */
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    /**
+     * Represents the token's type, lexeme, and literal as a string.
+     *
+     * @return A string representation of the object.
+     */
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
