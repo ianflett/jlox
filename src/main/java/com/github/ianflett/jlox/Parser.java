6c4c01fded2243d81401ec0f2b23104727096957
Ian Flett <ian.flett@gmail.com>
2024-03-15 18:24:19 +0000

    Implement naive parser layout

--- src/main/java/com/github/ianflett/jlox/Parser.java
+++ src/main/java/com/github/ianflett/jlox/Parser.java
@@ -1,43 +1,86 @@
 package com.github.ianflett.jlox;
 
-import jdk.jshell.spi.ExecutionControl;
+import static com.github.ianflett.jlox.TokenType.*;
 
 import java.util.List;
 
-import static com.github.ianflett.jlox.TokenType.*;
-
+/** Recursive decent parser that consumes tokens to produce abstract syntax tree. */
 public class Parser {
+
+    /** List of tokens to process. */
     private final List<Token> tokens;
+
+    /** Index of current token being parsed. */
     private int current = 0;
 
+    /**
+     * Constructs parser.
+     *
+     * @param tokens Tokens to parse.
+     */
     Parser(List<Token> tokens) {
         this.tokens = tokens;
     }
 
-    private Expr expression () {
+    /**
+     * Parses expression grammar rule.
+     *
+     * @return {@link Expr}ession or {@link #equality()}.
+     */
+    private Expr expression() {
         return equality();
     }
 
+    /**
+     * Parses equality grammar rule.
+     *
+     * @return Equality {@link Expr}ession or {@link #comparison()}.
+     */
     private Expr equality() {
         return comparison();
     }
 
+    /**
+     * Parses comparison grammar rule.
+     *
+     * @return Comparison {@link Expr}ession or {@link #term()}.
+     */
     private Expr comparison() {
         return term();
     }
 
+    /**
+     * Parses term grammar rule.
+     *
+     * @return Term {@link Expr}ession or {@link #factor()}.
+     */
     private Expr term() {
         return factor();
     }
 
+    /**
+     * Parses factor grammar rule.
+     *
+     * @return Factor {@link Expr}ession or {@link #unary()}.
+     */
     private Expr factor() {
         return unary();
     }
 
+    /**
+     * Parses unary grammar rule.
+     *
+     * @return Unary {@link Expr}ession or {@link #primary()}.
+     */
     private Expr unary() {
         return primary();
     }
 
+    /**
+     * Parses primary grammar rule.
+     *
+     * @return {@link Expr.Literal} or {@link Expr.Grouping}.
+     */
     private Expr primary() {
         throw new UnsupportedOperationException();
     }
